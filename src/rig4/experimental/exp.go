a818ebbd4c7e3c7e81d94b37235fd2fe0c4dbc08
ralf <ralfoide@gmail.com>
2016-06-23 23:54:47 +0200

    Fix duplicated header CSS.

--- src/rig4/experimental/exp.go
+++ src/rig4/experimental/exp.go
@@ -101,58 +101,6 @@ var EXP_HTML_TEMPLATE = `<html lang="en">
   font-size: 13px;
 }
 
-.demo-ribbon {
-  width: 100%;
-  height: 40vh;
-  background-color: #3F51B5;
-  -webkit-flex-shrink: 0;
-      -ms-flex-negative: 0;
-          flex-shrink: 0;
-  background-image: url("test_banner_img.jpg");
-  background-position: center;
-  background-size: 100% auto;
-  background-repeat: no-repeat;
-}
-
-.demo-main {
-  margin-top: -10vh;
-  -webkit-flex-shrink: 0;
-      -ms-flex-negative: 0;
-          flex-shrink: 0;
-}
-
-.demo-header .mdl-layout__header-row {
-  padding-left: 40px;
-}
-
-.demo-container {
-  max-width: 1600px;
-  width: calc(100% - 16px);
-  margin: 0 auto;
-}
-
-.demo-content {
-  border-radius: 2px;
-  padding: 40px 56px 80px; /* top side bottom */
-  margin-bottom: 80px;
-}
-
-.demo-layout.is-small-screen .demo-content {
-  padding: 40px 28px;
-}
-
-.demo-content h3 {
-  margin-top: 48px;
-}
-
-.demo-footer {
-  padding-left: 40px;
-}
-
-.demo-footer .mdl-mini-footer--link-list a {
-  font-size: 13px;
-}
-
 .demo-footer2 {
     display: -webkit-flex;
     display: -ms-flexbox;
