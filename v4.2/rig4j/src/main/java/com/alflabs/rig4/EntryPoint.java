dac36cd9c74aa3a2d77bb8a177b00bfb19f875b9
ralf <ralfoide@gmail.com>
2017-11-15 00:07:41 -0800

    v4.2: rig4j skeleton

---
+++ v4.2/rig4j/src/main/java/com/alflabs/rig4/EntryPoint.java
@@ -0,0 +1,7 @@
+package com.alflabs.rig4;
+
+public class EntryPoint {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
---
+++ v4.2/rig4j/src/test/java/com/alflabs/rig4/EntryPointTest.java
@@ -0,0 +1,16 @@
+package com.alflabs.rig4;
+
+import org.junit.Before;
+import org.junit.Test;
+
+public class EntryPointTest {
+    @Before
+    public void setUp() throws Exception {
+        System.out.println("EntryPointTest.setUp");
+    }
+
+    @Test
+    public void test1() throws Exception {
+        System.out.println("EntryPointTest.test1");
+    }
+}
