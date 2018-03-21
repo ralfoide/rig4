faea72af6686264450c62fb585ee7968d818890e
ralf <ralfoide@gmail.com>
2018-03-20 22:52:47 -0700

    v4.1: Remove old sources. Available using tag "v4.1"

--- v4.1/_build.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-if [[ -z "$GOPATH" ]]; then . ./_setup.sh; fi
-if [[ ! -d "bin" ]]; then mkdir -p bin; fi
-function _mv() { for i in $* ; do if [[ -f "$i" ]]; then mv -vf "$i" bin/ ; fi; done; }
-go build -v $@ src/rig4.go && _mv rig4 rig4.exe
-
