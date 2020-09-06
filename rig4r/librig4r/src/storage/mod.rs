1cc90cd0862e1580ed9575cdf346211b93c6b756
ralf <ralfoide@gmail.com>
2020-09-06 14:48:56 -0700

    Rig4r: Lib with one example.

--- rig4r/librig4r/src/storage/mod.rs
+++ rig4r/librig4r/src/storage/mod.rs
@@ -1,6 +1,9 @@
 
-pub mod hash_store;
+mod hash_store;
+pub use hash_store::HashStore;
 
 pub fn the_store() { 
     println!("the store");
+    let h = HashStore::new(6);
+    h.v();
 }
--- rig4r/rig4r-dl/src/main.rs
+++ rig4r/rig4r-dl/src/main.rs
@@ -1,10 +1,9 @@
 
 use rig4r::storage::*;
-use rig4r::storage::hash_store::HashStore;
 
 fn main() {
     println!("Hello, world!");
     the_store();
-    let x = HashStore::new(5);
+   let x = HashStore::new(5);
     x.v();
 }
