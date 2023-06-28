0a50ab60ac3e0961ccd057a073e8fe7d50e63684
ralf <ralfoide@gmail.com>
2023-06-28 17:50:41 +0200

    Rig4k: Run.sh with fatJar.

--- _run.sh
+++ _run.sh
@@ -1,1 +1,6 @@
-./gradlew run --console=plain $@
+#!/usr/bin/bash
+if [[ -z "$1" ]]; then
+  ./gradlew run --console=plain
+else
+  ./gradlew fatJar --console=plain && java -jar build/libs/rig4-0.1-SNAPSHOT-all.jar $@
+fi
