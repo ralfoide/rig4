e7e2d734de1b88c00136d4cc756858f82b9a0f00
ralf <ralfoide@gmail.com>
2023-07-17 22:11:34 -0700

    Rig4j: Remove temp debug printf.

--- rig4j/src/main/java/com/alflabs/rig4/gdoc/GDocHelper.java
+++ rig4j/src/main/java/com/alflabs/rig4/gdoc/GDocHelper.java
@@ -337,8 +337,6 @@ public class GDocHelper {
         int w = image.getWidth();
         int h = image.getHeight();
 
-        // TEMP DEBUG for resize
-        mLogger.d(TAG, "@@ DEBUG: target: " + width + " x " + height + ", image: " + w + " x " + h);
         if (width > 0 && height <= 0) {
             height = (int) Math.round(h * (double) w / (double) width);
         } else if (width <= 0 && height > 0) {
@@ -347,8 +345,6 @@ public class GDocHelper {
             width = w;
             height = h;
         }
-        // TEMP DEBUG for resize
-        mLogger.d(TAG, "@@ DEBUG: reisze to: " + width + " x " + height);
 
         ByteArrayOutputStream pngStream = new ByteArrayOutputStream();
         ByteArrayOutputStream jpgStream = new ByteArrayOutputStream();
